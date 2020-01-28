adfea4a60c8b744f73e0c68cb0ff1174a94a427c
Hen443 <henula09106@gmail.com>
2020-01-28 15:44:02 +0400

    New chat Prog3

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
+var messages = [];
+
+app.use(express.static("."));
+
+app.get('/', function (req, res) {
+   res.redirect('index.html');
+});
+
+
+
+io.on('connection', function (socket) {
+    for(var i in messages) {
+      io.sockets.emit("display message", messages[i]);
+    }
+    socket.on("send message", function (data) {
+        messages.push(data);
+        io.sockets.emit("display message", data);
+    });
+ });
+ 
+
+ server.listen(3000, function(){
+     console.log('Server stared on 3000')
+ })
