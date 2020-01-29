e9de6c6ec35663129cc366407dca52b8ff6982f1
Hen443 <henula09106@gmail.com>
2020-01-29 20:07:25 +0400

    bug fixed on socket emit everyone but me

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     for(var i in messages) {
-      io.sockets.emit("display message", messages[i]);
+      socket.emit("display message", messages[i]);
     }
     socket.on("send message", function (data) {
         messages.push(data);
